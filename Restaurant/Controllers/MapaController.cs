a48779288ae5dfd59d2a27dba65b3f19155b188d
budy77 <ruddy.marquina@gmail.com>
2015-11-08 18:29:55 -0400

    3 commit
    se agrego la funcion del mapa para los restaurant

---
+++ Restaurant/Controllers/MapaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant.Models;
+
+namespace Restaurant.Controllers
+{
+    public class MapaController : Controller
+    {
+        private restaurantEntities bd = new restaurantEntities();
+        // GET: Mapa
+        public ActionResult Index()
+        {
+            List<int> idRestaurant = new List<int>();
+            List<string> nombres = new List<string>();
+            List<decimal> latitud = new List<decimal>();
+            List<decimal> longitud = new List<decimal>();
+
+            var restaurant = from r in bd.restaurant  select r;
+            foreach (var o in restaurant)
+            {
+                idRestaurant.Add(o.idRestaurant);
+                nombres.Add(o.nombreRestaurant);
+                latitud.Add((decimal)o.latitud);
+                longitud.Add((decimal)o.longitud);
+            }
+
+            ViewBag.id = idRestaurant;
+            ViewBag.nombres = nombres;
+            ViewBag.latitud = latitud;
+            ViewBag.longitud = longitud;
+            return View();
+        }
+    }
+}
