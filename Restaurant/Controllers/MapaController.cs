51b4ae716f8d6f4f3433c7eb39bda1ea6d45ed89
budy77 <ruddy.marquina@gmail.com>
2015-12-29 19:29:46 -0400

    4 commit
    completo requerimiento 2

--- Restaurant/Controllers/MapaController.cs
+++ Restaurant/Controllers/MapaController.cs
@@ -17,6 +17,8 @@ namespace Restaurant.Controllers
             List<string> nombres = new List<string>();
             List<decimal> latitud = new List<decimal>();
             List<decimal> longitud = new List<decimal>();
+            List<string> datos = new List<string>();
+            List<string> des = new List<string>();
 
             var restaurant = from r in bd.restaurant  select r;
             foreach (var o in restaurant)
@@ -25,12 +27,16 @@ namespace Restaurant.Controllers
                 nombres.Add(o.nombreRestaurant);
                 latitud.Add((decimal)o.latitud);
                 longitud.Add((decimal)o.longitud);
+                datos.Add(o.datosRestaurant);
+                des.Add(o.descripcionRestaurant);
             }
 
             ViewBag.id = idRestaurant;
             ViewBag.nombres = nombres;
             ViewBag.latitud = latitud;
             ViewBag.longitud = longitud;
+            ViewBag.datos = datos;
+            ViewBag.des = des;
             return View();
         }
     }
